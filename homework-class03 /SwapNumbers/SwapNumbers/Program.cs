860ed3ca0867845b012722f4ca84bc7e5a943de7
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-03-16 22:32:35 +0100

    Add files via upload

---
+++ homework-class03 /SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number1, number2;
+Console.WriteLine("The first input number is:");
+ number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("The second input number is:");
+ number2 = int.Parse(Console.ReadLine());
+(number1, number2) = (number2, number1);
+Console.WriteLine("After swaping" + " " + "First number is:"
+ + " "  + number1 + " " + "Second number is:" +" " + number2);
+
