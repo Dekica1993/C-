14ed8ea3c5bb94bbc8fda4010f1bbfc52a90d72b
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-03-16 22:34:54 +0100

    Add files via upload

---
+++ C#(Homeworks)/homework-class03 /SwapNumbers/StudentGroup/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string[] G1 = { "Dejan", "Elena", "Biljana", "Ana", "Bojan" };
+string[] G2 = { "Marija", "Monika", "Tamara", "Ema", "Teo" };
+Console.WriteLine("Enter student group:(There are 1 or 2)");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+if (inputNumber == 1)
+{
+    foreach (string groupName in G1)
+    {
+        Console.WriteLine(groupName);
+
+    }
+    
+}
+ else if (inputNumber == 2)
+{
+    foreach (string groupName in G2)
+    {
+        Console.WriteLine(groupName);
+
+    }
+
+
+}
+
---
+++ C#(Homeworks)/homework-class03 /SwapNumbers/sumOfEven/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+int[] arrays = new int[6];
+int sum = 0;
+
+for (int i = 0; i < 6;   i ++)
+{
+    Console.WriteLine("enter number");
+    arrays[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+foreach (int num in arrays)
+{
+    if (num % 2 == 0)
+        sum += num;
+
+}
+
+
+
+
+Console.WriteLine($"The result is: {sum}");
