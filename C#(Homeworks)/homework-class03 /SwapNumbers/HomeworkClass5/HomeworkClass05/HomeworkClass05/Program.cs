e189c921500793c3ec633b3a79d8cbb83a1e2df7
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-03-22 19:12:28 +0100

    Add files via upload

---
+++ C#(Homeworks)/homework-class03 /SwapNumbers/HomeworkClass5/HomeworkClass05/HomeworkClass05/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine("Enter an input");
+string substringEx = Convert.ToString(Console.ReadLine());
+string newSubstring = substringEx.Substring(substringEx.Length - 5);
+Console.WriteLine(newSubstring);
+
+
+
