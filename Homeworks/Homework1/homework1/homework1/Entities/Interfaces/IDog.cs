36f7907f0586711daea2be1c39c9449424bfdd18
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-04-07 00:04:36 +0200

    Add files via upload

---
+++ Homeworks/Homework1/homework1/homework1/Entities/Interfaces/IDog.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace homework1.Entities.Interfaces
+{
+	public interface IDog
+	{
+		string Bark();
+	}
+}
+
---
+++ Homeworks/Homework1/homework1/homework1/Program.cs
@@ -0,0 +1,7 @@
+﻿using homework1.Entities;
+
+Dog brko = new Dog("Dekica", "Labrador", 4, "Black");
+Console.WriteLine(brko.Bark()); 
+brko.PrintAnimal();
+Console.WriteLine(brko.Name); 
+
