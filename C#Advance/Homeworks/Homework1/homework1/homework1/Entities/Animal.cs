a5cf3f4f3ce7a05f791d6e1510eb167a4f413adf
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-04-07 00:06:32 +0200

    Add files via upload

---
+++ C#Advance/Homeworks/Homework1/homework1/homework1/Entities/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using homework1.Entities.Interfaces;
+
+namespace homework1.Entities
+{
+	public abstract  class Animal : IAnimal
+	{
+		public string Name { get; set; }
+		public string Breed { get; set; }
+		public int Age { get; set; }
+		public string Color { get; set; }
+
+        public abstract void PrintAnimal();
+
+		public Animal (string name,string breed,int age,string color)
+		{
+			Name = name;
+			Breed = breed;
+			Age = age;
+			Color = color;
+		}
+
+		
+
+        
+
+        
+    }
+
+
+
+	
+    
+}
+
---
+++ C#Advance/Homeworks/Homework1/homework1/homework1/Entities/Interfaces/ICat.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace homework1.Entities.Interfaces
+{
+	public interface ICat
+	{
+		void Eat(string food);
+	}
+}
+
