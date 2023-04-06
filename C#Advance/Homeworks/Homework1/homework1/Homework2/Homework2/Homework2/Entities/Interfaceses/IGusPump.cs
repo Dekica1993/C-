63a360ade402a05df9479fef090179e9def22af7
Dekica1993 <119583123+Dekica1993@users.noreply.github.com>
2023-04-07 00:07:48 +0200

    Delete C#Advance/Homeworks/Homework1/homework1/Homework2/Homework2 directory

--- C#Advance/Homeworks/Homework1/homework1/Homework2/Homework2/Homework2/Entities/Interfaceses/IGusPump.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Homework2.Entities.Interfaceses
-{
-	public interface IGusPump
-	{
-		void PumpGas(string Cars, string Trucks);
-	}
-}
-
--- C#Advance/Homeworks/Homework1/homework1/Homework2/Homework2/Homework2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Homework2.Entities;
-
-CarCenter honda = new CarCenter();
-honda.WashCars("Bugatti");
-Console.WriteLine(honda.Truck("Lada")); 
-
-Truck lada = new Truck();
-Console.WriteLine(lada.CheckVehicle("Ferari", "Fiat"));
-Console.WriteLine(lada.FixVehicle("Honda", "Volvo")); 
